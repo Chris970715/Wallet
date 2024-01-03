39163a9eaa4719bc16184cdbe562a216f7cd18f8
Christopher Gyubum Kim <kgb2872@gmail.com>
2024-01-02 22:57:46 -0500

    main_1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Web3Modal from "web3modal";
+const ethers = require("ethers");
+
+//import {CoinbaseWalletSDK} from "@coinbase/wallet-sdk";
+
+const providerOptions = {
+  /*
+  coinbasewallet : {
+    package : CoinbaseWalletSDK,
+    options: {
+      appName: "Web3Modal Demo",
+      infuraId: {3: 1}
+    }
+  }*/
+}
 
 function App() {
+
+  async function connectWallet() {
+    try{
+
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        //providerOptions,
+      });
+
+      const web3ModalInstance = await web3Modal.connect();
+      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+      console.log(web3ModalProvider);
+
+    } catch(error) {
+      console.error(error.message);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Wallet Connect</h1>
+        <button onClick={connectWallet}>Connect Wallet</button>
+
       </header>
     </div>
   );
